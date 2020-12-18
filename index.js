d4f1b9d1874dad615e1108c6a101842774bdf2e0
Promise Fru <promisefru1@gmail.com>
2020-12-18 14:54:03 +0100

    mysql db connected with sequelize

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const Sequelize = require("sequelize");
 const routes = require('./routes/route.js')
+const cred = require("./cred.json")
+
 const app = express();
+
+// create connection : new Sequelize(database, username, password, {host, dialect})
+const sequelize = new Sequelize(cred.DATABASE, cred.USERNAME, cred.PASSWORD, {
+    host: cred.HOST,
+    dialect: "mysql"
+});
+
+// test connection with the .authenticate() function
+sequelize.authenticate()
+    .then(() => {
+        console.log("connected");
+    })
+    .catch((err) => {
+        console.error(err)
+    })
 
 app.use(routes);
 
